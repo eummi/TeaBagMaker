6ba1ee42fa1a36c025dda7a3595ffa1b44247b98
Gihun Ham <qkrdmsal0312@gmail.com>
2019-05-21 11:18:37 +0900

    초기 커밋.

---
+++ TeaBagMaker/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace TeaBagMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.time = new System.Windows.Forms.Label();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "2019.05.21 3608 박은미";
+            // 
+            // comboBox
+            // 
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(13, 13);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(242, 20);
+            this.comboBox.TabIndex = 1;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.ComboBox_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Location = new System.Drawing.Point(12, 100);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(57, 12);
+            this.time.TabIndex = 2;
+            this.time.Text = "차 시간 : ";
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(13, 40);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(242, 57);
+            this.button.TabIndex = 3;
+            this.button.Text = "담그기";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 156);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ TeaBagMaker/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.VisualBasic;
+
+namespace TeaBagMaker
+{
+    public partial class Form1 : Form
+    {
+        string[] SList = new string[] { "차", "홍차", "녹차", "루이보스차", "국화차" };
+        string orgStr = "";
+        int CountOrgNum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < SList.Length; i++)
+            {
+                this.comboBox.Items.Add(SList[i]);
+            }
+            this.orgStr = time.Text;
+            if (comboBox.Items.Count > 0)
+            {
+                this.comboBox.SelectedIndex = 0;
+            }
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (this.comboBox.Text == "홍차")
+            {
+                this.time.Text = "2분";
+            }
+            else if (this.comboBox.Text == "녹차")
+            {
+                
+            }
+            else if (this.comboBox.Text == "루이보스차")
+            {
+
+            }
+            else if (this.comboBox.Text == "국화차")
+            {
+
+            }
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (CountOrgNum < 1)
+            {
+                this.timer1.Enabled = false;
+                this.time.Text = "";
+                MessageBox.Show("티백을 건지세요!", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                CountOrgNum--;
+                this.time.Text = Convert.ToString(CountOrgNum);
+            }
+
+        }
+
+        private void ComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.comboBox.Text != "차")
+            {
+                this.time.Text = this.comboBox.Text;
+            }
+
+        }
+    }
+}
